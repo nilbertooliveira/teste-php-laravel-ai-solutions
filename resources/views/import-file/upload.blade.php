b1f2fac2f5c7cb7f2948aab639b9049bbc10e962
Nilberto Oliveira <nilbertooliveira.ti@gmail.com>
2024-07-13 01:29:54 -0300

    Ajuste de erros de login.

--- resources/views/import-file/upload.blade.php
+++ resources/views/import-file/upload.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-auto">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
                         <button type="button" class="btn btn-primary" data-bs-toggle="modal"
