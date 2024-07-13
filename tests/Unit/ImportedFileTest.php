de7ec72ea8cee1aecd44baed1c0f52cf4413480d
Nilberto Oliveira <nilbertooliveira.ti@gmail.com>
2024-07-12 23:26:17 -0300

    Testes

--- tests/Unit/ImportedFileTest.php
+++ tests/Unit/ImportedFileTest.php
@@ -2,33 +2,65 @@
 
 namespace Tests\Unit;
 
-use App\Application\Services\ImportFileService;
-use App\Domain\Interfaces\Repositories\IImportFilesRepository;
-use App\Infrastructure\Database\Models\Document;
-use App\Infrastructure\Database\Models\ImportedFile;
-use App\Rules\MaxContentFieldSize;
-use App\Rules\Uppercase;
-use App\Validators\ConteudoValidator;
-use Illuminate\Filesystem\FilesystemManager;
-use Illuminate\Validation\ValidationException;
-use Mockery;
 use PHPUnit\Framework\TestCase;
-use Storage;
 
 class ImportedFileTest extends TestCase
 {
 
-    public function test ()
+    private string $path = 'data';
+    private string $disk = 'data';
+    private string $fileName = 'test.json';
+    private array $file;
+
+    protected function setUp(): void
     {
+        $contents = file_get_contents("storage/$this->path/$this->fileName");
+        $this->file = json_decode($contents, true);
 
-        $fail = fn($message) => $this->assertEquals($message, 'The :attribute must be uppercase.');
-
-        $validator = new Uppercase();
-
-        $validator->validate('myparam', 'myvalue', $fail);
-
-        // i need to assert here, $fail contains the failure message
+        parent::setUp();
     }
 
 
+    public function test_max_content_field_size(): void
+    {
+        $this->assertIsArray($this->file);
+        $this->assertArrayHasKey('documentos', $this->file);
+
+        foreach ($this->file['documentos'] as $documento) {
+            $this->assertArrayHasKey('categoria', $documento);
+            $this->assertArrayHasKey('titulo', $documento);
+            $this->assertArrayHasKey('conteúdo', $documento);
+
+            $this->validatedMaxContentFieldSize($documento['conteúdo']);
+
+            $this->validatedContentFix($documento['categoria'], $documento['titulo']);
+        }
+    }
+
+    public function test_content_fix(): void
+    {
+        foreach ($this->file['documentos'] as $documento) {
+            $this->validatedContentFix($documento['categoria'], $documento['titulo']);
+        }
+    }
+
+    private function validatedMaxContentFieldSize(string $content, int $size = 50)
+    {
+        if (strlen($content) > $size) {
+            $this->fail("O campo conteúdo excede $size caracteres.");
+        }
+
+        $this->assertTrue(true);
+    }
+
+    private function validatedContentFix(string $category, string $title)
+    {
+        if ($category == 'Remessa' && !str_contains($title, 'semestre')) {
+            $this->fail("Registro inválido semestre não encontrado!");
+        } elseif ($category == 'Remessa Parcial' && !str_contains($title, 'Janeiro')) {
+            $this->fail("Registro inválido, falta meses");
+        }
+
+        $this->assertTrue(true);
+    }
 }
