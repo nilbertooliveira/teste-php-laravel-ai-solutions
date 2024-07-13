67a62172b61794e1b1f33956bfc5fd409040ffb0
Nilberto Oliveira <nilbertooliveira.ti@gmail.com>
2024-07-13 02:03:44 -0300

    Ajuste de erros de login.

--- app/Infrastructure/Database/Models/FailedJobs.php
+++ app/Infrastructure/Database/Models/FailedJobs.php
@@ -7,13 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * @property int id
- * @property string uuid
- * @property string connection
- * @property string queue
- * @property string payload
- * @property string exception
- * @property DateTime failed_at
+ * @property int $id
+ * @property string $uuid
+ * @property string $connection
+ * @property string $queue
+ * @property string $payload
+ * @property string $exception
+ * @property DateTime $failed_at
  */
 class FailedJobs extends Model
 {
